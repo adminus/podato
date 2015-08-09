e7c33cdca16c083c94a4d3aa11a9a9e6b573832e
Frederik Creemers <frederikcreemers@gmail.com>
2015-08-09 18:26:16 +0200

    converted users.auth.models module to rethinkdb.

--- webapp/users/auth/models.py
+++ webapp/users/auth/models.py
@@ -1,22 +1,32 @@
 import logging
 
 from flask import abort
-from webapp.db import db
+from webapp.db import Model
 
 from webapp.users.auth import facebook_api
 from webapp.users.auth.providers import TwitterProvider
 
-class ProvidedIdentity(db.EmbeddedDocument):
-    """Model that represents a user identity as provided by a 3rd party identity provider."""
-    provider = db.StringField(required=True)
-    user_id = db.StringField(required=True)
-    access_token = db.StringField(required=True)
+class ProvidedIdentity(Model):
+    attributes = ["provider", "user_id", "access_token"]
+
+    def __init__(self, provider=None, user_id=None, access_token=None):
+        self.provider = provider
+        self.user_id=user_id
+        self.access_token = access_token
 
 
-class ProviderTokenHolder(object):
+class ProviderTokenHolder(Model):
     """This is a mixin for User, which stores the auth tokens of 3rd party providers like Facebook or Google"""
 
-    provided_identities = db.EmbeddedDocumentListField(ProvidedIdentity)
+    attributes = ["provided_identities"]
+
+    def __init__(self, provided_identities=None):
+        self.provided_identities is provided_identities or []
+
+    @classmethod
+    def from_dict(cls, d):
+        d["provided_identities"] = [ProvidedIdentity(**d2) for d2 in d["provided_identities"]]
+        return cls(**d)
 
     def add_provided_identity(self, provider, user_id, access_token):
         # If the user already has an identity from the given platform with the given id,
@@ -29,7 +39,7 @@ class ProviderTokenHolder(object):
         for identity in self.provided_identities:
             if identity.provider == provider and identity.user_id == user_id:
                 identity.access_token = access_token
-                self.put()
+                self.save()
                 logging.debug("identity already found on user. Updating access token.")
                 return
 
@@ -38,7 +48,9 @@ class ProviderTokenHolder(object):
             user_id=user_id,
             access_token=access_token
         )
-        self.modify(push__provided_identities=prid)
+        self.run(self.table.get(self.id).update(
+            lambda user: user["provided_identities"].append(prid.to_dict())
+        ))
         logging.debug("Added provided identity.")
 
     def get_provider_token(self, provider, user_id=None):
@@ -52,12 +64,11 @@ class ProviderTokenHolder(object):
     @classmethod
     def get_by_provided_identity(cls, provider, user_id):
         """Gets the User associated with the given provided identity."""
-        user = cls.objects(provided_identities__provider=provider, provided_identities__user_id=user_id).first()
-        logging.debug("Tried to get user by provided identity. %r" % {
-            "provider": provider,
-            "user_id": user_id,
-            "found": user
-        })
+        user = cls.run(cls.get_table().filter(
+            lambda user: user["provided_identities"].contains(
+                lambda identity: identity["provider"] == provider & identity["user_id"] == user_id
+            )
+        ))
         return user
 
     @classmethod
@@ -82,7 +93,7 @@ class ProviderTokenHolder(object):
         user = cls.get_by_provided_identity("facebook", fb_user["id"])
         if not user:
             user = cls.create(fb_user["name"], fb_user["email"], facebook_api.get_avatar(fb_user["id"]))
-            user.put()
+            user.save()
             logging.debug("Saved new user.")
         user.add_provided_identity("facebook", fb_user["id"], access_token)
         return user
@@ -96,7 +107,7 @@ class ProviderTokenHolder(object):
         user = cls.get_by_provided_identity("twitter", tw_user.id_str)
         if not user:
             user = cls.create(tw_user.screen_name, None, tw_user.profile_image_url_https)
-            user.put()
+            user.save()
             logging.debug("Saved new user.")
         user.add_provided_identity("twitter", tw_user.id_str, access_token)
         return user
