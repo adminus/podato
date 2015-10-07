17f7b7bde6fe1d7b2537a05161f67e1f6197f5ee
Frederik Creemers <frederikcreemers@gmail.com>
2015-10-07 15:25:01 +0200

    show an image in the play bar, and only show the pay bar while playing an episode.

--- webapp/static/jssrc/components/playbar/playbar.jsx
+++ webapp/static/jssrc/components/playbar/playbar.jsx
@@ -1,18 +1,40 @@
 const React = require("react");
 const Link = require("react-router").Link;
+const ListenerMixin = require("alt/mixins/ListenerMixin");
 
+const Image = require("../common/image.jsx");
+
+const PlaybackStore = require("../../stores/playback-store");
 
 const PlayBar = React.createClass({
+    mixins: [ListenerMixin],
     render(){
+        if(!this.state.episode){
+            return null;
+        }
         return (
             <nav className="fixed bottom-0 left-0 right-0 bg-red white px4" style={{height:"2.5rem"}}>
                 <div className="container flex flex-stretch">
+                    <Image src={this.state.episode.image} style={{height: "2.5rem"}} />
                     <button className="button button-red"><i className="el el-backward" /></button>
                     <button className="button button-red"><i className="el el-play" /></button>
                     <button className="button button-red"><i className="el el-forward" /></button>
                 </div>
             </nav>
         )
+    },
+    getInitialState(){
+        return {
+            episode: PlaybackStore.getState().currentEpisode
+        }
+    },
+    componentWillMount(){
+        this.listenTo(PlaybackStore, this.storeDidChange);
+    },
+    storeDidChange(){
+        this.setState({
+            episode: PlaybackStore.getState().currentEpisode
+        });
     }
 });
 
--- webapp/static/jssrc/stores/playback-store.js
+++ webapp/static/jssrc/stores/playback-store.js
@@ -4,9 +4,7 @@ const PlaybackActions = require("../actions/playback-actions");
 const PlaybackStore = flux.createStore(class UsersStore{
     constructor(){
         this.currentEpisode = null;
-        this.currentEpisode = null;
         this.playing = false;
-        this.playbackState = {};
         this.bindActions(PlaybackActions);
     }
 
