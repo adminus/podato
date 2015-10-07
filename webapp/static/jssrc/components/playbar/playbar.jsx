3e0ef4cf03c6c369eb139d8d9f38f5e192af5775
Frederik Creemers <frederikcreemers@gmail.com>
2015-10-07 15:43:09 +0200

    Hooked up play/pause button in playbar.

--- webapp/static/jssrc/components/playbar/playbar.jsx
+++ webapp/static/jssrc/components/playbar/playbar.jsx
@@ -5,6 +5,7 @@ const ListenerMixin = require("alt/mixins/ListenerMixin");
 const Image = require("../common/image.jsx");
 
 const PlaybackStore = require("../../stores/playback-store");
+const PlaybackActions = require("../../actions/playback-actions");
 
 const PlayBar = React.createClass({
     mixins: [ListenerMixin],
@@ -12,12 +13,16 @@ const PlayBar = React.createClass({
         if(!this.state.episode){
             return null;
         }
+        var playButton = <button className="button button-red" onClick={this.play}><i className="el el-play" /></button>
+        if(this.state.playing){
+            playButton = <button className="button button-red" onClick={this.pause}><i className="el el-pause"/></button>
+        }
         return (
             <nav className="fixed bottom-0 left-0 right-0 bg-red white px4" style={{height:"2.5rem"}}>
                 <div className="container flex flex-stretch">
                     <Image src={this.state.episode.image} style={{height: "2.5rem"}} />
                     <button className="button button-red"><i className="el el-backward" /></button>
-                    <button className="button button-red"><i className="el el-play" /></button>
+                    {playButton}
                     <button className="button button-red"><i className="el el-forward" /></button>
                 </div>
             </nav>
@@ -25,6 +30,7 @@ const PlayBar = React.createClass({
     },
     getInitialState(){
         return {
+            playing: PlaybackStore.getState().playing,
             episode: PlaybackStore.getState().currentEpisode
         }
     },
@@ -33,8 +39,15 @@ const PlayBar = React.createClass({
     },
     storeDidChange(){
         this.setState({
+            playing: PlaybackStore.getState().playing,
             episode: PlaybackStore.getState().currentEpisode
         });
+    },
+    play(){
+        PlaybackActions.resume();
+    },
+    pause(){
+        PlaybackActions.pause();
     }
 });
 
