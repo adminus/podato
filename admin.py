ff547226bea24ab81a0e2dc8ed965bd13487c50e
Frederik Creemers <frederikcreemers@gmail.com>
2015-11-01 23:24:01 +0100

    add RedthinkDB repl command to admin.py

--- admin.py
+++ admin.py
@@ -1,28 +1,37 @@
-from webapp.podcasts.crawler import update_podcasts
-from webapp.app import app
-from webapp import cache
-import sys
+from webapp.main import app
 
-def list_cache_keys():
-    p = sys.argv[2]
-    print "Finding keys matching %s..." % p
-    print cache.list_keys(p)
+with app.app_context():
+    from webapp.podcasts.crawler import update_podcasts
+    from webapp import cache
+    from webapp.db import get_connection, r
 
-commands = {
-    "update_podcasts": update_podcasts,
-    "flush_cache": cache.flush,
-    "list_cache_keys": list_cache_keys,
-    "get_cache_key": cache.get
-}
+    import sys, code
 
-subcommand = sys.argv[1]
-if not subcommand in commands:
-    print "No command named %s, supported commands: %s" % (subcommand, ", ".join(commands.keys()))
-else:
-    command = commands[subcommand]
-    args = sys.argv[2:]
-    arity = command.func_code.co_argcount
-    args = args[:arity]
-    value = command(*args)
-    print "returned %s: %s" % (type(value), value)
-    print "done!"
+    def list_cache_keys():
+        p = sys.argv[2]
+        print "Finding keys matching %s..." % p
+        print cache.list_keys(p)
+
+    def repl():
+        get_connection().repl()
+        code.InteractiveConsole(locals=globals()).interact()
+
+    commands = {
+        "update_podcasts": update_podcasts,
+        "flush_cache": cache.flush,
+        "list_cache_keys": list_cache_keys,
+        "get_cache_key": cache.get,
+        "db_repl": repl
+    }
+
+    subcommand = sys.argv[1]
+    if not subcommand in commands:
+        print "No command named %s, supported commands: %s" % (subcommand, ", ".join(commands.keys()))
+    else:
+        command = commands[subcommand]
+        args = sys.argv[2:]
+        arity = command.func_code.co_argcount
+        args = args[:arity]
+        value = command(*args)
+        print "returned %s: %s" % (type(value), value)
+        print "done!"
